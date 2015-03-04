16a1731642262b6f2e35c1fa6af8b9bb4a7f2191
Inti Pedroso <inti.pedroso@fraunhofer.cl>
2015-03-04 17:34:09 -0300

    added code to  get the genomes names from bam files and to start recoving the alignment scores

--- read_assignment.py
+++ read_assignment.py
@@ -1,22 +1,44 @@
 import pysam
 import numpy as np
+#import tables as hdf5
+import pandas as pd 
+import subprocess
 
 
-samfile = pysam.AlignmentFile("/media/TeraData/ipedroso/METAGENOMICS/ANALYSES/ALN/simLC/Zymomonas_mobilis_CP4___NRRL_B_14023_uid229874.sorted.bam", "rb")
+aln_folder = "/media/TeraData/ipedroso/METAGENOMICS/ANALYSES/ALN/simLC/"
 
-# get number of genomes and of reads on file
-nG = len(samfile.header["SQ"])
-info_seq_G = samfile.header["SQ"]
-nR = samfile.count()
+proc = subprocess.Popen(["".join(("ls ",aln_folder,"/Z*bam"))], stdout=subprocess.PIPE, shell=True)
+(out, err) = proc.communicate()
 
-Q = np.zeros((nR,nG),dtype=np.float16)
+bam_files = out.split()
+all_genomes = np.hstack([pysam.AlignmentFile(bam, "rb").references for bam in bam_files])
 
 
-reads_id = {}
-read_counter = 0
-for read in samfile.fetch():
-    if read.qname not in reads_id:
-	reads_id[read.qname] = read_counter
-	read_counter += 1
-    Q[reads_id[read.qname],read.rname] = read.mapq
+Q = pd.DataFrame(columns=all_genomes,dtype=np.float16)
+
+flush = 100
+
+for bam in bam_files:
+	samfile = pysam.AlignmentFile(bam, "rb")
+	nR = samfile.count()
+	local_Q = pd.DataFrame(np.zeros((flush , samfile.nreferences)),columns=samfile.references,dtype=np.float16)
+	reads_id = {}
+	read_counter = 0
+	flush_counter = 0
+	for read in samfile.fetch():
+    		if read.qname not in reads_id:
+			reads_id[read.qname] = read_counter
+			read_counter += 1
+
+		local_Q.ix[flush_counter,read.rname] = read.mapq
+		flush_counter += 1
+		
+		if flush_counter > 99:
+			# here we need to add local_Q to Q with the align methods of 
+			flush_counter = 0
+			print local_Q.head()
+			local_Q = pd.DataFrame(np.zeros((flush , samfile.nreferences)),columns=samfile.references,dtype=np.float16)
+			continue
+
+
 
