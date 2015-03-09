2dd223cc44674217e9530fc7cbba5d34d89585ec
Inti Pedroso <inti.pedroso@fraunhofer.cl>
2015-03-09 15:53:34 -0300

    Major changes. faster merging of data across genomes using dictionaries

--- read_data_into_hdf5.py
+++ read_data_into_hdf5.py
@@ -5,7 +5,9 @@ import pandas as pd
 import subprocess
 import h5py
 import os
+from progressbar import ProgressBar, Percentage, Bar, ETA, FileTransferSpeed, RotatingMarker
 
+widgets = ['Proggres: ', Percentage(), ' ', Bar(), ' ', ETA(), ' ', FileTransferSpeed()] 
 
 dt_str_vlen = h5py.special_dtype(vlen=str)
 
@@ -13,6 +15,12 @@ dtype_stringVlen_float16 =[('name1', dt_str_vlen), ('name2', 'float16')]
 
 
 ######################################################################################
+def openBamfile(bam):
+	samfile = pysam.AlignmentFile(bam, "rb")
+        species_name = os.path.basename(bam).replace(".sorted.bam","")
+        nR = samfile.count()
+        nG = samfile.nreferences
+	return samfile, species_name, nR, nG
 
 def rescale_samMAPQ(mapq):
 	return 1 - np.power(10,mapq/-10)
@@ -36,13 +44,7 @@ aln_folder = "/media/TeraData/ipedroso/METAGENOMICS/ANALYSES/ALN/simLC/"
 
 proc = subprocess.Popen(["".join(("ls ",aln_folder,"/Esch*bam"))], stdout=subprocess.PIPE, shell=True)
 (out, err) = proc.communicate()
-
 bam_files = out.split()
-#all_genomes=pd.concat([ pd.DataFrame([pysam.AlignmentFile(bam, "rb").count(),
-#			pysam.AlignmentFile(bam, "rb").nreferences, 
-#			os.path.basename(bam).replace(".sorted.bam","")],
-#		      index=["R","G","species_name"]).T	
-#		 for bam in bam_files])
 
 h5_file = h5py.File("mytestfile.hdf5", "w")
 
@@ -50,23 +52,51 @@ genomes = h5_file.create_group("genomes")
 
 #all_genomes.apply(lambda (nR,nG,species_name): create_genome_group_and_Q_tables(genomes,species_name,nR,nG),axis=1)
 
-for bam in bam_files:
-	print ctime(), bam
+print ctime(), "Gathering read mapped to all genomes"
+master_index = dict()
+genome_index = dict()
+j = 0
+pbar = ProgressBar(widgets=widgets, maxval=len(bam_files)*10).start()
+for bam_counter,bam in enumerate(bam_files):
+	#print ctime(), bam
 	try:
-		samfile = pysam.AlignmentFile(bam, "rb")
-		species_name = os.path.basename(bam).replace(".sorted.bam","")
-		nR = samfile.count()
-		nG = samfile.nreferences
+		samfile, species_name, nR, nG = openBamfile(bam)
 		
 		create_genome_group_and_Q_tables(genomes,species_name,nR,nG,samfile.references)
-	
-		read_counter = 0
-		for read in samfile.fetch():
+		genome_index[j] = species_name	
+		#read_counter = 0
+		for read_counter,read in enumerate(samfile.fetch()):
 			genomes[species_name]["Q"][read_counter,read.rname] = rescale_samMAPQ( read.mapq )
 			genomes[species_name]["read_names"][read_counter,:] = read.qname
-			read_counter += 1
+			#read_counter += 1
+			if read.qname not in master_index.keys():
+                        	master_index[ read.qname ] = dict()
+                	if species_name in master_index[read.qname]:
+                        	master_index[ read.qname ][ j ] = np.append( master_index[ read.qname ][ species_name ] , read_counter)
+                	else:
+                        	master_index[ read.qname ][ j ] = read_counter
+
+		j += 1
 	except:
 		print "not working"
+	pbar.update(10*bam_counter+1)
+pbar.finish()
 
 
+nR, nG = len(master_index), len(genome_index)
+
+Q_all      = h5_file.create_dataset("Q_all"     ,(nR,nG),dtype='float')
+Q_all_rows = h5_file.create_dataset("Q_all_rows",(nR,),dtype=dt_str_vlen)
+Q_all_cols = h5_file.create_dataset("Q_all_cols",(nG,),dtype=dt_str_vlen)
+
+
+print ctime(), "Combining mapping results across genomes"
+pbar = ProgressBar(widgets=widgets, maxval=len(master_index.keys())*10).start()
+for i,read in enumerate(master_index):
+	for j in master_index[read]: 
+		Q_all[i,j] = genomes[ genome_index[j] ]["Q"][  master_index[ read ][ j ]  , 0 ] 
+	pbar.update(10*i+1)
+pbar.finish()
+
+print ctime(), "Done"
 
--- setup_python_env.sh
+++ setup_python_env.sh
@@ -1,2 +1,2 @@
-conda create --yes -n metagenomics pysam pip numpy h5py pandas pytables scipy accelerate mkl cython ipython ipython-notebook matplotlib mpi4py 
+conda create --yes -n metagenomics pysam pip numpy h5py pandas pytables scipy accelerate mkl cython ipython ipython-notebook matplotlib mpi4py progressbar 
 
